4366c4beffb6fc7d782227cd8c90fa618073cf5c
ivanalsc <ivanasoscordero@gmail.com>
2020-10-21 17:48:31 -0300

    Updates

---
+++ precache-manifest.82f52e2269f93cd00f69eda5e2189413.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4183d8f436bba2e21dd213c005296a11",
+    "url": "/myshop/index.html"
+  },
+  {
+    "revision": "69cf31ebac9a5bfd773f",
+    "url": "/myshop/static/css/main.747524aa.chunk.css"
+  },
+  {
+    "revision": "cc66d6ba71338a7c4f26",
+    "url": "/myshop/static/js/2.76db63c8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/myshop/static/js/2.76db63c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cf31ebac9a5bfd773f",
+    "url": "/myshop/static/js/main.0c548ce3.chunk.js"
+  },
+  {
+    "revision": "27b6c70bd43b641cc3ac",
+    "url": "/myshop/static/js/runtime-main.7ff129b4.js"
+  }
+]);
