2593fd874f721dd69e732b4f45e243d47f1d80fe
ivanalsc <ivanasoscordero@gmail.com>
2020-10-21 19:59:20 -0300

    Icons fixed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import cart from '../img/black-cart.svg';
+
 
 export const Modal = ({closeModal}) => {
     return (
         <div className='modal'>
             <div className='mensaje'>
                 <button onClick={closeModal}>X</button>
-                <img src='../img/black-cart.svg' alt='' />
+                <img src={cart} alt='' />
                 <p>Se agregó el producto al carrito</p>
                 <p>Llevás un total de: $1500</p>
 
